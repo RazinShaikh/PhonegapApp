8dbd09511173cda4883aecb40a26e6928fd6e426
RazinShaikh <razinshaikh@outlook.com>
2017-08-14 12:36:23 +0530

    Images are now loaded from server

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -10,11 +10,22 @@ $$(document).on('deviceready', function() {
         datatype: 'json',
         type: 'GET',
         cache: true,
-        url: 'http://192.168.0.102:8000/activity/',
+        url: 'http://127.0.0.1:8000/activity.json',
         success: function(data) {
             var articles = JSON.parse(data);
             addArticlesLatest(articles);
             addArticlesHot(articles);
+        }
+    });
+    $$.ajax({
+        datatype: 'json',
+        type: 'GET',
+        cache: true,
+        url: 'http://127.0.0.1:8000/thumbs.json',
+        success: function(data) {
+            var thumbs = JSON.parse(data);
+            addThumbnailsLatest(thumbs);
+            addThumbnailsHot(thumbs);
         }
     });
 });
@@ -22,19 +33,29 @@ $$(document).on('deviceready', function() {
 
 function addArticlesLatest(data) {
     for (var i = 0; i < data.length; i++) {
-        htmlStr = "<div class=\"card myCard\"><div class=\"card-content\"><div class=\"card-content-inner\"><div class=\"row\"><div class=\"col-50\"><img src=\"img/1.jpg\" height=\"90\" width=\"147\"></div><div class=\"col-50\">" + data[i].title + "</div></div></div></div></div>"
+        htmlStr = "<div class=\"card myCard\"><div class=\"card-content\"><div class=\"card-content-inner\"><div class=\"row\"><div class=\"col-50\"><img id=\"img_latest_"+i+"\" src=\"\" height=\"90\" width=\"147\"></div><div class=\"col-50\">" + data[i].title + "</div></div></div></div></div>"
         $$(pageTab2).append(htmlStr);
     }
 }
 
 function addArticlesHot(data) {
     for (var i = 0; i < data.length; i++) {
-        htmlStr = "<div class=\"card myCard\"><div class=\"card-content\"><div class=\"card-content-inner\"><div class=\"row\"><div class=\"col-50\"><img src=\"img/2.jpg\" height=\"90\" width=\"147\"></div><div class=\"col-50\">" + data[i].title + "</div></div></div></div></div>"
+        htmlStr = "<div class=\"card myCard\"><div class=\"card-content\"><div class=\"card-content-inner\"><div class=\"row\"><div class=\"col-50\"><img id=\"img_hot_"+i+"\" src=\"\" height=\"90\" width=\"147\"></div><div class=\"col-50\">" + data[i].title + "</div></div></div></div></div>"
         $$(pageTab1).append(htmlStr);
     }
 }
 
+function addThumbnailsLatest(data) {
+    for (var i = 0; i < data.length; i++) {
+        $$('#img_latest_'+i).attr("src", "data:image/jpg;base64," + data[i]);
+    }
+}
 
+function addThumbnailsHot(data) {
+    for (var i = 0; i < data.length; i++) {
+        $$('#img_hot_'+i).attr("src", "data:image/jpg;base64," + data[i]);
+    }
+}
 
 
 var mySwiper = myApp.swiper('.swiper-container', {
